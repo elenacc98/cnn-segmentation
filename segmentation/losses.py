97ede4e336a237f8f3cd69a5d209d4708fba328c
Alberto <47320499+AlbiFag@users.noreply.github.com>
2020-11-15 21:37:46 +0100

    Created 2D and 3D losses

--- segmentation/losses.py
+++ segmentation/losses.py
@@ -4,6 +4,8 @@ in segmentation tasks.
 """
 
 from segmentation.metrics import MeanDice
+from segmentation.utils import calc_dist_map_batch, calc_dist_map_batch_2D, calc_dist_map, calc_dist_map_2D
+from segmentation.utils import count_class_voxels, get_loss_weights
 from keras import backend as K
 import numpy as np
 import tensorflow as tf
@@ -158,83 +160,74 @@ class MeanDiceLoss(MeanDice):
 
 
 
-def Weighted_DiceBoundary_Loss(numClasses, alpha):
-
-    def calc_dist_map(seg):
-        """
-        Computes distance map using scipy function
-        Args:
-            seg: 2D or 3D binary array to compute the distance map
-        Returns:
-            res: distance map
-        """
-
-        res = np.zeros_like(seg)
-        posmask = seg.astype(np.bool)
-
-        if posmask.any():
-            negmask = ~posmask
-            res = distance(negmask) * negmask + (distance(posmask) - 1) * posmask
-        return res
-
-    def calc_dist_map_batch(y_true):
-        """
-        Pass 5D or 4D label volumes to calc_dist_map and return the corresponding numpy array distance map to loss
-        function.
-        Args:
-            y_true: ground truth tensor of dimensions (class, batch_size, rows, columns, slices) or
-            (class, batch_size, rows, columns)
-        Returns:
-            array of distance map of the same dimension of input tensor
-        """
-        y_true_numpy = y_true.numpy()
-        dist_batch = np.zeros_like(y_true_numpy)
-        for c in range(numClasses):
-            temp_y = y_true_numpy[c]
-            for i, y in enumerate(temp_y):
-                dist_batch[c, i] = calc_dist_map(y)
-        return np.array(dist_batch).astype(np.float32)
-
-    def count_class_voxels(labels, nVoxels):
-        """
-        Counts total number of voxels for each class in the batch size.
-        input is supposed to be 4 or 5-dimensional: (class, batch, rows, columns) or
-        (class, batch, rows, columns, slices).
-        Args:
-            labels: ground truth tensor of dimensions (class, batch_size, rows, columns, slices) or
-            (class, batch_size, rows, columns)
-            nVoxels: total number of voxels.
-        Returns:
-            out: list with number of voxel per class
-        """
-        out = [0] * numClasses
-        out[0] = 0
-        for c in range(1, numClasses):
-            out[c] = tf.math.count_nonzero(labels[c])
-        first_term = tf.cast(nVoxels, 'int64')
-        second_term = tf.reduce_sum(out)
-        out[0] = tf.subtract(first_term, second_term)
-        return out
-
-    def get_loss_weights(labels, nVoxels):
-        """
-        Compute loss weights for each class.
-        Args:
-            labels: ground truth tensor of dimensions (class, batch_size, rows, columns, slices) or
-            (class, batch_size, rows, columns)
-            nVoxels: total number of voxels.
-        Returns:
-            1D tf.tensor of len = numClasses containing weights for each class
-        """
-
-        numerator_1 = count_class_voxels(labels, nVoxels)
-        numerator = tf.multiply(1.0 / nVoxels, numerator_1)
-        subtract_term = tf.subtract(1.0, numerator)
-        return tf.multiply(1.0 / (numClasses - 1), subtract_term)
+def Weighted_DiceBoundary_Loss_3D(numClasses, alpha):
 
     def multiclass_3D_class_weighted_dice_boundary_loss(y_true, y_pred):
         """
         Compute 3D multiclass class weighted dice loss function.
+        Args:
+            y_true: ground truth tensor of dimensions (class, batch_size, rows, columns, slices) or
+            (class, batch_size, rows, columns)
+            y_pred: softmax probabilities predicting classes. Shape must be the same as y_true.
+        """
+
+        if len(y_true.shape) == 4:
+            axisSum = (1, 2)
+            y_pred = tf.transpose(y_pred, [3, 0, 1, 2])
+            y_true = tf.transpose(y_true, [3, 0, 1, 2])
+        elif len(y_true.shape) == 5:
+            axisSum = (1, 2, 3)
+            y_pred = tf.transpose(y_pred, [4, 0, 1, 2, 3])
+            y_true = tf.transpose(y_true, [4, 0, 1, 2, 3])
+        else:
+            print("Could not handle input dimensions.")
+            return
+
+        # Now dimensions are --> (numClasses, batchSize, Rows, Columns, Slices)
+        y_true = tf.cast(y_true, tf.float32)
+        y_pred = tf.cast(y_pred, tf.float32)
+        nVoxels = tf.size(y_true) / numClasses
+        nVoxels = tf.cast(nVoxels, tf.float32)
+
+        mean_over_classes = tf.zeros((1,))
+        # Get loss weights
+        loss_weights = get_loss_weights(y_true, nVoxels)
+        # Loop over each class
+        for c in range(numClasses):
+            y_true_c = y_true[c]
+            y_pred_c = y_pred[c]
+            numerator = tf.scalar_mul(2.0, tf.reduce_sum(tf.multiply(y_true_c, y_pred_c), axis = axisSum))
+            denominator = tf.add(tf.reduce_sum(y_true_c, axis = axisSum), tf.reduce_sum(y_pred_c, axis = axisSum))
+            class_loss_weight = loss_weights[c]
+
+            mean_over_classes = tf.add(mean_over_classes,
+                                       tf.multiply(class_loss_weight,
+                                       tf.divide(numerator, denominator)))
+
+        SDM = tf.py_function(func=calc_dist_map_batch_2D,
+                             inp=[y_true],
+                             Tout=tf.float32)
+        boundary_loss = tf.multiply(tf.reduce_sum(tf.multiply(SDM, y_pred)), 1.0/nVoxels)
+
+        return alpha * tf.subtract(1.0, mean_over_classes) + (1-alpha) * boundary_loss
+
+    return multiclass_3D_class_weighted_dice_boundary_loss
+
+
+def Weighted_DiceBoundary_Loss_2D(numClasses, alpha):
+    """
+    DiceBoundary wrapper function.
+    Args:
+        numClasses: number of classes
+        alpha: parameter to weight contribution of dice and boundary loss
+
+    Returns: multiclass_2D_weighted_dice_boundary_loss
+
+    """
+
+    def multiclass_2D_weighted_dice_boundary_loss(y_true, y_pred):
+        """
+        Compute 2D multiclass weighted dice + boundary with distance map loss function.
         Args:
             y_true: ground truth tensor of dimensions (class, batch_size, rows, columns, slices) or
             (class, batch_size, rows, columns)
@@ -281,10 +274,10 @@ def Weighted_DiceBoundary_Loss(numClasses, alpha):
 
         return alpha * tf.subtract(1.0, mean_over_classes) + (1-alpha) * boundary_loss
 
-    return multiclass_3D_class_weighted_dice_boundary_loss
+    return multiclass_2D_weighted_dice_boundary_loss
 
 
-def Weighted_CatCross_Loss(numClasses):
+def Weighted_CatCross_Loss_2D(numClasses):
     """
     Categorical crossentropy wrapper function between y_pred tensor and a target tensor.
     Arguments:
@@ -295,42 +288,8 @@ def Weighted_CatCross_Loss(numClasses):
         ValueError: if `axis` is neither -1 nor one of the axes of `output`.
     """
 
-    def calc_dist_map(seg):
-        """ Computes distance map using scipy function.
-        Args:
-            seg: 2D or 3D binary array to compute the distance map
-        Returns:
-            res: distance map
-        """
 
-        res = np.zeros_like(seg)
-        posmask = seg.astype(np.bool)
-
-        if posmask.any():
-            negmask = ~posmask
-            res = distance(negmask) * negmask + (distance(posmask) - 1) * posmask
-        return res
-
-    def calc_dist_map_batch(y_true):
-        """
-        Pass 5D or 4D label volumes to calc_dist_map and return the corresponding numpy array distance map to loss
-        function.
-        Args:
-            y_true: ground truth tensor of dimensions (class, batch_size, rows, columns, slices) or
-            (class, batch_size, rows, columns)
-        Returns:
-            array of distance map of the same dimension of input tensor
-        """
-
-        y_true_numpy = y_true.numpy()
-        dist_batch = np.zeros_like(y_true_numpy)
-        for i,y in enumerate(y_true_numpy):
-            for c in range(numClasses):
-                dist_batch[i,:,:,:,c] = calc_dist_map(y[:,:,:,c])
-        return np.array(dist_batch).astype(np.float32)
-
-
-    def categorical_cross_entropy(y_true, y_pred):
+    def categorical_cross_entropy_2D(y_true, y_pred):
         """
         Computes categorical cross entropy weighted by distance weighted map.
         Args:
@@ -369,4 +328,58 @@ def Weighted_CatCross_Loss(numClasses):
 
         return -math_ops.reduce_sum(DWM * y_true * math_ops.log(y_pred))/tf.cast(tot_voxels, tf.float32)
 
-    return categorical_cross_entropy
+    return categorical_cross_entropy_2D
+
+
+def Weighted_CatCross_Loss_3D(numClasses):
+    """
+    Categorical crossentropy wrapper function between y_pred tensor and a target tensor.
+    Arguments:
+        numClasses: number of classes
+    Returns:
+        categorical_cross_entropy function
+    Raises:
+        ValueError: if `axis` is neither -1 nor one of the axes of `output`.
+    """
+
+
+    def categorical_cross_entropy_3D(y_true, y_pred):
+        """
+        Computes categorical cross entropy weighted by distance weighted map.
+        Args:
+            y_true: A tensor of the same shape as `y_pred`.
+            y_pred: A tensor resulting from a softmax
+        Returns:
+            categorical crossentropy value
+
+        """
+
+        y_true = ops.convert_to_tensor_v2(y_true)
+        y_pred = ops.convert_to_tensor_v2(y_pred)
+        y_true = tf.cast(y_true, tf.float32)
+        y_pred = tf.cast(y_pred, tf.float32)
+
+        tot_voxels = tf.size(y_true)/numClasses
+        tot_voxels = tf.cast(tot_voxels, tf.float32)
+
+        y_true.shape.assert_is_compatible_with(y_pred.shape)
+
+        SDM = tf.py_function(func=calc_dist_map_batch_2D,
+                             inp=[y_true],
+                             Tout=tf.float32)
+
+        epsilon = backend_config.epsilon
+        gamma = 8
+        sigma = 10
+
+        # Exponential transformation of the Distance transform
+        DWM = 1 + gamma * tf.math.exp(tf.math.negative(SDM)/sigma)
+        # scale preds so that the class probas of each sample sum to 1
+        y_pred = y_pred / math_ops.reduce_sum(y_pred, axis=-1, keepdims=True)
+        # Compute cross entropy from probabilities.
+        epsilon_ = constant_op.constant(epsilon(), y_pred.dtype.base_dtype)
+        y_pred = clip_ops.clip_by_value(y_pred, epsilon_, 1. - epsilon_)
+
+        return -math_ops.reduce_sum(DWM * y_true * math_ops.log(y_pred))/tf.cast(tot_voxels, tf.float32)
+
+    return categorical_cross_entropy_3D
--- segmentation/utils.py
+++ segmentation/utils.py
@@ -1,3 +1,122 @@
 """
 Utils functions.
 """
+
+from scipy.ndimage import distance_transform_edt as distance
+import numpy as np
+import tensorflow as tf
+
+
+def calc_dist_map(seg):
+    """
+    Computes distance map using scipy function
+    Args:
+        seg: 2D or 3D binary array to compute the distance map
+    Returns:
+        res: distance map
+    """
+
+    res = np.zeros_like(seg)
+    posmask = seg.astype(np.bool)
+
+    if posmask.any():
+        negmask = ~posmask
+        res = distance(negmask) * negmask + (distance(posmask) - 1) * posmask
+    return res
+
+
+def calc_dist_map_2D(seg):
+    """
+    Computes 2D distance map of all slices using scipy function
+    Args:
+        seg: 2D or 3D binary array to compute the distance map
+    Returns:
+        res: distance map
+    """
+
+    res = np.zeros_like(seg)
+    posmask = seg.astype(np.bool)
+
+    for i in range(seg.shape[2]):
+        pos = posmask[:, :, i]
+        if pos.any():
+            neg = ~pos
+            res[:, :, i] = distance(neg) * neg + (distance(pos) - 1) * pos
+    return res
+
+
+def calc_dist_map_batch(y_true):
+    """
+    Pass 5D or 4D label volumes to calc_dist_map and return the corresponding numpy array distance map to loss
+    function.
+    Args:
+        y_true: ground truth tensor of dimensions (class, batch_size, rows, columns, slices) or
+        (class, batch_size, rows, columns)
+    Returns:
+        array of distance map of the same dimension of input tensor
+    """
+    y_true_numpy = y_true.numpy()
+    dist_batch = np.zeros_like(y_true_numpy)
+    for c in range(numClasses):
+        temp_y = y_true_numpy[c]
+        for i, y in enumerate(temp_y):
+            dist_batch[c, i] = calc_dist_map(y)
+    return np.array(dist_batch).astype(np.float32)
+
+
+def calc_dist_map_batch_2D(y_true):
+    """
+    Pass 5D or 4D label volumes to calc_dist_map and return the corresponding numpy array distance map to loss
+    function.
+    Args:
+        y_true: ground truth tensor of dimensions (class, batch_size, rows, columns, slices) or
+        (class, batch_size, rows, columns)
+    Returns:
+        array of distance map of the same dimension of input tensor
+    """
+    y_true_numpy = y_true.numpy()
+    dist_batch = np.zeros_like(y_true_numpy)
+    for c in range(numClasses):
+        temp_y = y_true_numpy[c]
+        for i, y in enumerate(temp_y):
+            dist_batch[c, i] = calc_dist_map_2D(y)
+    return np.array(dist_batch).astype(np.float32)
+
+
+def count_class_voxels(labels, nVoxels):
+    """
+    Counts total number of voxels for each class in the batch size.
+    input is supposed to be 4 or 5-dimensional: (class, batch, rows, columns) or
+    (class, batch, rows, columns, slices).
+    Args:
+        labels: ground truth tensor of dimensions (class, batch_size, rows, columns, slices) or
+        (class, batch_size, rows, columns)
+        nVoxels: total number of voxels.
+    Returns:
+        out: list with number of voxel per class
+    """
+    out = [0] * numClasses
+    out[0] = 0
+    for c in range(1, numClasses):
+        out[c] = tf.math.count_nonzero(labels[c])
+    first_term = tf.cast(nVoxels, 'int64')
+    second_term = tf.reduce_sum(out)
+    out[0] = tf.subtract(first_term, second_term)
+    return out
+
+
+def get_loss_weights(labels, nVoxels):
+    """
+    Compute loss weights for each class.
+    Args:
+        labels: ground truth tensor of dimensions (class, batch_size, rows, columns, slices) or
+        (class, batch_size, rows, columns)
+        nVoxels: total number of voxels.
+    Returns:
+        1D tf.tensor of len = numClasses containing weights for each class
+    """
+
+    numerator_1 = count_class_voxels(labels, nVoxels)
+    numerator = tf.multiply(1.0 / nVoxels, numerator_1)
+    subtract_term = tf.subtract(1.0, numerator)
+    return tf.multiply(1.0 / (numClasses - 1), subtract_term)
