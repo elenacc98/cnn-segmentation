0d32c3964e87f9bc179552af83f601cf3ac81fba
Alberto <47320499+AlbiFag@users.noreply.github.com>
2020-12-12 11:41:45 +0100

    bug fixes

--- segmentation/utils.py
+++ segmentation/utils.py
@@ -620,10 +620,10 @@ def build_MINI_MTL(input_shape, filters, numClasses, i):
 
     out_edge = Conv3D(numClasses, (1, 1, 1), padding='same')(x_edge)
     out_edge = Softmax(axis=-1)(out_edge)
-    out_edge = UpSampling3D(i+1, name='out_edge_{}'.format(i))(out_edge)
+    out_edge = UpSampling3D(pow(2,i), name='out_edge_{}'.format(i))(out_edge)
     out_mask = Conv3D(numClasses, (1, 1, 1), padding='same')(x_mask)
     out_mask = Softmax(axis=-1)(out_mask)
-    out_mask = UpSampling3D(i+1, name='out_mask_{}'.format(i))(out_mask)
+    out_mask = UpSampling3D(pow(2,i), name='out_mask_{}'.format(i))(out_mask)
 
     out_mtl = Concatenate()([x_mask, x_edge])
     out_mtl = Conv3D(filters, (1, 1, 1), padding='same')(out_mtl)
