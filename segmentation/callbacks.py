23d20e10f58e5798b4c538092a8d2f2438b0c724
Alberto <47320499+AlbiFag@users.noreply.github.com>
2021-03-18 19:02:10 +0100

    edit in MetricsPlpot

--- segmentation/callbacks.py
+++ segmentation/callbacks.py
@@ -116,8 +116,8 @@ class MetricsPlot(Callback):
         sns.set(style='darkgrid')
         for metric in self.metrics:
             if metric in logs.keys():
-                sns.lineplot(x=range(0, len(self.data)), y=metric, data=self.data, ci=None,
-                             label=metric, linewidth=3)
+                sns.lineplot(x=range(0,len(self.data)),y=metric,data=self.data,ci=None,
+                            label=metric, linewidth=3)
 
         ax.set_xlabel("Epochs", fontsize=16)
         ax.set_ylabel("", fontsize=16)
